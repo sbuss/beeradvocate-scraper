6ba646db44575164f2dbfba6e96822961ad5f97e
Steven Buss <sbuss@counsyl.com>
2013-06-30 20:40:37 -0700

    Start project.

---
+++ beeradvocate/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for beeradvocate project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'beeradvocate'
+
+SPIDER_MODULES = ['beeradvocate.spiders']
+NEWSPIDER_MODULE = 'beeradvocate.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'beeradvocate (+http://www.yourdomain.com)'
